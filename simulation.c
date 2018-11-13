d4fc8295c98f4d1ee6f8bae8a7c2e621e6652dcb
shantanu <shantanu.gupta33@gmail.com>
2018-11-13 00:10:36 -0600

    Revert "Replacing mallocs with constant memory allocations"
    This reverts commit 2eead86a45c598e6907a6cef1454324b95044b30.

--- simulation.c
+++ simulation.c
@@ -9,51 +9,7 @@
 #include <stdbool.h>
 #include <time.h>
 
-// #include "setup.h"
-#define NEAREST_MULTIPLE(n, d) (n - n % d)
-
-const size_t MAX_SAMPLES = 10000;	// maximum number of samples we can handle
-
-const double f0 = 4.0;				// sample rate of original signal, in Hz
-const double T0 = 1.0 / 4.0;		// 1 / f0, so in seconds
-
-const double T_window = 60;			// Window length for reconstruction algorithm,
-									// in seconds
-const size_t N_window = 240;		// #samples in that window
-									//  = NEAREST_MULTIPLE(T_window / T0, 4)
-
-const unsigned int CF = 12;			// compression factor
-const size_t M = 20;				// compressed window length = N_window / CF
-
-const double LASSO_lambda = 0.01;	// regularization parameter for LASSO
-const double CD_diff_thresh = 0.01;	// stopping threshold for coordinate descent
-									// algorithm
-
-// from lasso
-// double A_norm2[N_window];
-double A_norm2[240];
-// double y_hat[M];
-double y_hat[20];
-// double r[M];
-double r[20];
-
-// from ppg
-// double psi[N_window * N_window];
-double psi[240 * 240];
-// double psiT[N_window * N_window];
-double psiT[240 * 240];
-// double A[M * N_window];
-double A[20 * 240];
-// double y[M];
-double y[20];
-// double s[N_window];
-double s[240];
-// double xr[N_window];
-double xr[240];
-
-// from main
-// bool phi_flags[N_window];
-bool phi_flags[240];
+#include "setup.h"
 
 double max(double a, double b) {
 	return (a > b ? a : b) ;
@@ -81,8 +37,7 @@ void cd_lasso(double *y, double *A,
 				size_t M, size_t N, double lambda,
 				double *x_hat, double tol) {
 	// y M-dim, A is MxN, x_hat is Nx1
-	// double *A_norm2 = malloc(sizeof(double) * N);
-	// double A_norm2[N];
+	double *A_norm2 = malloc(sizeof(double) * N);
 	memset(A_norm2, 0, sizeof(double) * N);
 	for (int i = 0; i < M; ++i) {
 		for (int j = 0; j < N; ++j) {
@@ -94,11 +49,11 @@ void cd_lasso(double *y, double *A,
 		x_hat[i] = 0.5;
 	}
 
-	// double *y_hat = malloc(sizeof(double) * M);
+	double *y_hat = malloc(sizeof(double) * M);
 	// y_hat = A * x_hat
 	dot(A, x_hat, M, N, 1, y_hat);
 
-	// double *r = malloc(sizeof(double) * M);
+	double *r = malloc(sizeof(double) * M);
 	memcpy(r, y, sizeof(double) * M);
 	vecsub(y, y_hat, M, r);
 
@@ -142,9 +97,9 @@ void cd_lasso(double *y, double *A,
 		}
 	}
 
-	// free(r);
-	// free(y_hat);
-	// free(A_norm2);
+	free(r);
+	free(y_hat);
+	free(A_norm2);
 }
 
 void get_random_sample_flags_from_file(char *filepath, size_t N, bool *flags) {
@@ -198,13 +153,9 @@ void get_selected_rows(double *mat, bool *flags, int N, int m, double *out) {
 	}
 }
 
-/*
 void ppg(double *t0, double *X0, size_t N0,
 		 bool *phi_flags, const PPG_Params params,
 		 double *Xr) {
-*/
-void ppg(double *t0, double *X0, size_t N0,
-		 bool *phi_flags, double *Xr) {
 	/*
 	 * t0		:	sample instants (in s) 
 	 * X0		:	original samples (which we are trying to reconstruct)
@@ -215,15 +166,15 @@ void ppg(double *t0, double *X0, size_t N0,
 	 * Xr		:	where we should store the reconstructed signal (output)
 	 */
 	
-	// size_t N_window = params.N_window;
-	// size_t M = params.M;
+	size_t N_window = params.N_window;
+	size_t M = params.M;
 
 	// DCT basis matrix
 	// TODO: implement fast DCT so this isn't needed.
-	// double *psi = malloc(sizeof(double) * N_window * N_window);
+	double *psi = malloc(sizeof(double) * N_window * N_window);
 	// This is psi.T. Only keeping a separate copy because I am lazy.
 	// TODO: become less lazy. Also write something better.
-	// double *psiT = malloc(sizeof(double) * N_window * N_window);
+	double *psiT = malloc(sizeof(double) * N_window * N_window);
 	for (int k = 0; k < N_window; ++k) {
 		double factor = (2.0 / sqrt(N_window)) * (k == 0 ? 1.0 / sqrt(2) : 1.0);
 		for (int n = 0; n < N_window; ++n) {
@@ -232,12 +183,12 @@ void ppg(double *t0, double *X0, size_t N0,
 		}
 	}
 
-	// double *A = malloc(sizeof(double) * M * N_window);
+	double *A = malloc(sizeof(double) * M * N_window);
 	get_selected_rows(psiT, phi_flags, N_window, M, A);
 
-	// double *y = malloc(sizeof(double) * M);
-	// double *s = malloc(sizeof(double) * N_window);
-	// double *xr = malloc(sizeof(double) * N_window);
+	double *y = malloc(sizeof(double) * M);
+	double *s = malloc(sizeof(double) * N_window);
+	double *xr = malloc(sizeof(double) * N_window);
 
 	for (size_t t = 0; t < N0 - N_window + 1; t += N_window / 2) {
 		get_selected_elements(X0 + t, phi_flags, N_window, M, y);
@@ -248,12 +199,12 @@ void ppg(double *t0, double *X0, size_t N0,
 			   sizeof(double) * N_window / 2);
 	}
 	
-	// free(xr);
-	// free(s);
-	// free(y);
-	// free(A);
-	// free(psiT);
-	// free(psi);
+	free(xr);
+	free(s);
+	free(y);
+	free(A);
+	free(psiT);
+	free(psi);
 }
 
 /*
@@ -271,14 +222,11 @@ int main(int argc, char **argv) {
 	}
 
 	srand(time(NULL));
-	/*
 	PPG_Params params = get_ppg_params();
 	printf("f0: %.1lf\nT0: %.3lf\n", params.f0, params.T0);
 	printf("N_window: %ld\nM: %ld\n", params.N_window, params.M);
-	*/
 
-	// const size_t MAX_N = params.MAX_SAMPLES;
-	const size_t MAX_N = MAX_SAMPLES;
+	const size_t MAX_N = params.MAX_SAMPLES;
 	size_t N0 = 0;
 	double t0[MAX_N], X0[MAX_N];
 	double Xr[MAX_N];
@@ -297,8 +245,8 @@ int main(int argc, char **argv) {
 
 	printf("Read %ld samples.\n", N0);
 
-	// size_t N_window = params.N_window;
-	// bool *phi_flags = malloc(sizeof(bool) * N_window);
+	size_t N_window = params.N_window;
+	bool *phi_flags = malloc(sizeof(bool) * N_window);
 	get_random_sample_flags_from_file(argv[3], N_window, phi_flags);
 	// get_random_sample_flags(N_window, M, phi_flags);
 
@@ -307,8 +255,7 @@ int main(int argc, char **argv) {
 	printf("N0: %ld\n", N0);
 
 	// call ppg algorithm
-	// ppg(t0, X0, N0, phi_flags, params, Xr);
-	ppg(t0, X0, N0, phi_flags, Xr);
+	ppg(t0, X0, N0, phi_flags, params, Xr);
 
 	printf("Writing to file %s...\n", argv[2]);
 	FILE *fp_write = fopen(argv[2], "w");
@@ -321,6 +268,6 @@ int main(int argc, char **argv) {
 	}
 	fclose(fp_write);
 
-	// free(phi_flags);
+	free(phi_flags);
 	return 0;
 }
